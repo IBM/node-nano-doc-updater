3cc16d6fcdcf278fb0d5af21797b8f18382b3ff1
Chris Taylor <cntaylor@ca.ibm.com>
2016-10-17 10:29:52 -0400

    ct: Deconstruct the sample code from the README.
    - Break up the examples in the README so they're easier to follow.
    - Add tests to verify the examples in the README as part of CI.

---
+++ test/system/test-readme-example-flows.js
@@ -0,0 +1,165 @@
+var test = require("tape-catch"),
+    Promise = require("bluebird"),
+    resetTestDb = require("./lib/reset-test-db"),
+    nanoDocUpdater = require("../../");
+
+test("while testing the scenario from the README...", (t) => {
+    var db = null;
+    var designDocId = "_design/foo",
+        otherDocId = "foo",
+        designDoc1 = { language: "javascript", version: 1 };
+
+    /* eslint-disable */
+    var designDoc2 = {
+        language: "javascript",
+        version: 2,
+        views: {
+            count: {
+                map: 'function (d) { emit(null, 1); }',
+                reduce: 'function (k, v, r) { sum(v); }'
+            }
+        }
+    }; /* eslint-enable */
+
+    var updater = null;
+
+    resetTestDb()
+    .then((rawDb) => {
+        db = Promise.promisifyAll(rawDb);
+        updater = nanoDocUpdater();
+
+        t.comment("...after inserting a design document...");
+
+        return Promise.promisify(
+            updater
+            .db(db)
+            .newDoc(designDoc1)
+            .id(designDocId)
+            .shouldUpdate((existing, newDoc) => {
+                return !existing.version || existing.version < newDoc.version;
+            })
+            .merge(function (existing, newDoc) {
+                return newDoc;
+            })
+            .update,
+            updater
+        )();
+    })
+    .then(() => {
+        return db.getAsync(designDocId);
+    })
+    .then((r) => {
+        var doc = r[0];
+
+        delete doc._id;
+        delete doc._rev;
+
+        t.deepEqual(doc, designDoc1, "...does it equal what was inserted?");
+    })
+    .then(() => {
+        t.comment("...after updating it to a newer version (using the same update logic)...");
+
+        return Promise.promisify(
+            updater
+            .newDoc(designDoc2)
+            .update,
+            updater
+        )();
+    })
+    .then(() => {
+        return db.getAsync(designDocId);
+    })
+    .then((r) => {
+        var doc = r[0];
+
+        delete doc._id;
+        delete doc._rev;
+
+        t.deepEqual(doc, designDoc2, "...is it actually equivalent to the updated version, as we inserted it?");
+    })
+    .then(() => {
+        t.comment("...after inserting that design document to an arbitrary, different, _id...");
+
+        return Promise.promisify(
+            updater
+            .id(otherDocId)
+            .shouldUpdate(null)
+            .merge(null)
+            .update,
+            updater
+        )();
+    })
+    .then(() => {
+        return db.getAsync(otherDocId);
+    })
+    .then((r) => {
+        var doc = r[0];
+
+        delete doc._id;
+        delete doc._rev;
+
+        t.deepEqual(doc, designDoc2, "...does that same document exist at this arbitrary, different, _id?");
+    })
+    .then(() => {
+        t.comment("...after removing this new document...");
+        return Promise.promisify(
+            updater
+            .merge((existing) => {
+                existing._deleted = true;
+                return existing;
+            })
+            .update,
+            updater
+        )();
+    })
+    .then(() => {
+        return db.getAsync(otherDocId)
+        .then(() => {
+            t.fail("...a request to fetch the document succeeds.  It should have failed!");
+        })
+        .catch(NotFoundError, () => {
+            t.pass("...a request to fetch the document fails.  This is expected.");
+        });
+    })
+    .then(() => {
+        t.comment("...after removing the original design document...");
+
+        return Promise.promisify(
+            updater
+            .id(designDocId)
+            .merge((existing) => {
+                existing._deleted = true;
+                return existing;
+            })
+            .update,
+            updater
+        )();
+    })
+    .then(() => {
+        return db.getAsync(designDocId)
+        .then(() => {
+            t.fail("...a request to fetch the document succeeds.  It should have failed!");
+        })
+        .catch(NotFoundError, () => {
+            t.pass("...a request to fetch the document fails.  This is expected.");
+        });
+    })
+    .catch((e) => {
+        if (e instanceof Error) {
+            t.comment(e.stack);
+            t.fail("...an error occurred");
+        } else {
+            t.comment(e.stack);
+            t.fail("...an error occurred");
+        }
+
+        throw(e);
+    })
+    .finally(() => {
+        t.end();
+    });
+});
+
+function NotFoundError(e) {
+    return e && e.error === "not_found";
+}
---
+++ test/system/test-readme-synopsis-flow.js
@@ -0,0 +1,59 @@
+var test = require("tape-catch"),
+    Promise = require("bluebird"),
+    resetTestDb = require("./lib/reset-test-db");
+
+test("after execiting the steps in the Synopsis section" + 
+     "the README (insert a design document with custom)" +
+     "update criteria...", (t) => {
+    var db = null;
+    var designDocId = "_design/foo";
+    var designDoc = { language: "javascript", version: 1 };
+
+    resetTestDb()
+    .then((rawDb) => {
+        db = Promise.promisifyAll(rawDb);
+        return Promise.promisify(synopsisFlow)(db, designDocId, designDoc);
+    })
+    .then(() => {
+        return db.getAsync(designDocId);
+    })
+    .then((r) => {
+        var doc = r[0];
+        delete doc._id;
+        delete doc._rev;
+
+        t.deepEquals(doc, designDoc, "...and after fetching the design document manually, does it match what we inserted?");
+    })
+    .catch((e) => {
+        if (e instanceof Error) {
+            t.comment(e.stack);
+            t.fail("...an error occurred");
+        } else {
+            t.comment(e.stack);
+            t.fail("...an error occurred");
+        }
+
+        throw(e);
+    })
+    .finally(() => {
+        t.end();
+    });
+});
+
+function synopsisFlow(db, designDocId, designDoc, callback) {
+    var nanoDocUpdater = require("../../");
+    var updater = nanoDocUpdater(db);
+
+    updater
+    .db(db)
+    .existingDoc(null)
+    .newDoc(designDoc)
+    .id(designDocId)
+    .shouldUpdate((existing, newDoc) => {
+        return !existing.version || existing.version < newDoc.version;
+    })
+    .merge((existing, newDoc) => {
+        return newDoc;
+    })
+    .update(callback);
+}
